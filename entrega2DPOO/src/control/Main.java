8dea679eaf34454916ba94241ad5edbd2d9ee499
fardilaq <f.ardilaq@uniandes.edu.co>
2025-04-19 17:50:33 -0500

    Consola admin

--- entrega2DPOO/src/control/Main.java
+++ entrega2DPOO/src/control/Main.java
@@ -1,9 +1,39 @@
 package control;
-public class Main {
-    public static void main(String[] args) {
-        Parque parque = new Parque("Entrada principal");
 
-        ConsolaUsuario consola = new ConsolaUsuario(parque);
-        consola.iniciar();
+public class Main extends ConsolaBasica {
+
+    public static void main(String[] args) {
+        new Main().ejecutar();
+    }
+
+    public void ejecutar() {
+        Parque parque = new Parque("Taquilla principal");
+
+        if (parque.Usuarios == null)
+        {
+            parque.Usuarios = new java.util.HashMap<>();
+        }
+
+        String[] opciones = {
+            "Consola de Usuario",
+            "Consola de Administrador"
+        };
+
+        int opcion = mostrarMenu("Seleccione tipo de consola:", opciones);
+
+        if (opcion == 1)
+        {
+            ConsolaUsuario consolaUsuario = new ConsolaUsuario(parque);
+            consolaUsuario.iniciar();
+        }
+        else if (opcion == 2)
+        {
+            ConsolaAdministrador consolaAdmin = new ConsolaAdministrador(parque);
+            consolaAdmin.iniciar();
+        }
+        else
+        {
+            System.out.println("Opción no válida.");
+        }
     }
 }
